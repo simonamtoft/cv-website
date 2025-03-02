78c1777a8453e1d30fcd0cdfd931a7f5777b3c08
simonamtoft <39790702+simonamtoft@users.noreply.github.com>
2025-03-02 17:09:25 +0100

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -14,15 +14,10 @@ const About = () => {
         </div>
         <div className='about-text'>
           <p>
-            Growing up, my dad sparked my interest in mathematics and technology. I remember solving my first pair of
-            simultaneous equations with him in 6th grade and later taking apart small electronic toys to see how they
-            worked. That curiosity later led me to assembling my own PC and keyboards, giving me a better understanding
-            of how different components work together.
-          </p>
-          <p>
-            My early fascination led me to pursue a bachelor's degree in electrical engineering - a journey through math, 
-            physics, and programming that eventually inspired me to complete a master’s in engineering mathematical
-            modelling. This academic path taught me how to translate complex systems into practical, data-driven solutions.
+            Growing up, my dad sparked my interest in mathematics and technology, which led me to pursue a bachelor's
+            degree in electrical engineering - a journey through math, physics, and programming that eventually inspired
+            me to complete a master’s in engineering mathematical modelling. This academic path taught me how to
+            translate complex systems into practical, data-driven solutions.
           </p>
           <p>
             Today, I work as a Machine Learning Specialist at Stibo Systems, where I combine technical expertise with
